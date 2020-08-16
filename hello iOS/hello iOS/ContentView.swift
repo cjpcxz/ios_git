5a0b7ad06cf2986f63ce415e63feb22287a00d8a
cjp <1337811967@qq.com>
2020-08-16 18:50:04 +0800

    first commit

---
+++ hello iOS/hello iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello iOS
+//
+//  Created by new on 2020/8/16.
+//  Copyright © 2020 new. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
